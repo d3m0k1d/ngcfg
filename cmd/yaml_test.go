6dee6e482d61773a4c6914183cd5e6ca85600842
d3m0k1d <d3m0k1d1337@gmail.com>
2025-11-10 21:40:24 +0300

    Add simple test and rename docs file

--- cmd/yaml_test.go
+++ cmd/yaml_test.go
@@ -1,3 +1,30 @@
 package cmd
 
-import ()
+import (
+	"testing"
+)
+
+func TestGenNgconf(t *testing.T) {
+	var test_server Servers
+
+	test_server.Servers = append(test_server.Servers, Server{
+		Name:        "test",
+		Listen:      80,
+		ListenV6:    80,
+		Return:      "test",
+		Root_path_s: "/home/d3m0k1d/repo/ngcfg/test",
+		Charset:     "utf-8",
+		Locations: []Location{
+			{
+				Name:       "test",
+				Alias_path: "/home/d3m0k1d/repo/ngcfg/test",
+				Root_path:  "/home/d3m0k1d/repo/ngcfg/test",
+			},
+		},
+	})
+
+	_, err := GenNgconf(test_server)
+	if err != nil {
+		t.Error(err)
+	}
+}

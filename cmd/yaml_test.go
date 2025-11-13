39aa7ec63c516aa03e511d9fb0c8a575b256e988
d3m0k1d <d3m0k1d1337@gmail.com>
2025-11-13 21:37:04 +0300

    Update test for new struct and validate

--- cmd/yaml_test.go
+++ cmd/yaml_test.go
@@ -1,30 +1,61 @@
 package cmd
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestGenNgconf(t *testing.T) {
-	var test_server Servers
-
-	test_server.Servers = append(test_server.Servers, Server{
-		Name:        "test",
-		Listen:      80,
-		ListenV6:    80,
-		Return:      "test",
-		Root_path_s: "/home/d3m0k1d/repo/ngcfg/test",
-		Charset:     "utf-8",
-		Locations: []Location{
-			{
-				Name:       "test",
-				Alias_path: "/home/d3m0k1d/repo/ngcfg/test",
-				Root_path:  "/home/d3m0k1d/repo/ngcfg/test",
+	config := HttpConfig{
+		Http: Http{
+			ClientMaxBodySize: "100m",
+			KeepaliveTimeout:  65,
+			SendTimeout:       30,
+			Servers: []Server{
+				{
+					Name:        "test",
+					Listen:      80,
+					ListenV6:    80,
+					Return:      "301 https://example.com",
+					Root_path_s: "/home/d3m0k1d/repo/ngcfg/test",
+					Charset:     "utf-8",
+					Index:       "index.html",
+					SSL:         false,
+					Locations: []Location{
+						{
+							Name:       "test",
+							Alias_path: "/home/d3m0k1d/repo/ngcfg/test",
+							Root_path:  "/home/d3m0k1d/repo/ngcfg/test",
+						},
+					},
+				},
 			},
 		},
-	})
+	}
 
-	_, err := GenNgconf(test_server)
+	result, err := GenNgconf(config)
 	if err != nil {
-		t.Error(err)
+		t.Fatalf("GenNgconf failed: %v", err)
+	}
+
+	expectedStrings := []string{
+		"http {",
+		"client_max_body_size 100m;",
+		"keepalive_timeout 65;",
+		"send_timeout 30;",
+		"server_name test;",
+		"listen 80;",
+		"listen [::]:80;",
+		"charset utf-8;",
+		"root /home/d3m0k1d/repo/ngcfg/test;",
+		"return 301 https://example.com;",
+		"location /test {",
+	}
+
+	for _, expected := range expectedStrings {
+		if !strings.Contains(result, expected) {
+			t.Errorf("Expected output to contain %q, but it doesn't. Got: %s", expected, result)
+		}
 	}
 }
+
